dcfe945c92ed7ada94a7048933f11940ddc66d07
chanmca <chanhemgau@outlook.com>
2020-10-30 18:15:22 +0530

    completed filtered image url REST endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,63 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+  // function isValidImageURL(url: string): boolean {
+  //   console.log(url);
+  //   var regex = new RegExp('/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)');
+  //   console.log(regex.test(url));
+  //   return regex.test(url);
+  // }
+
   // GET filteredimage endpoint
-  app.get( "/filteredimage", async ( req, res ) => {
-    const image_url = req.params.image_url;
+  app.get("/filteredimage", async (req, res) => {
+    const isImageURL = require('image-url-validator');
+    let imageURL = req.query.image_url;
     //validate the image_url query
-    if (!image_url) { 
-      return res.send(500).send({ message: 'image_url is must, but not found' });
+    if (!imageURL) {
+      return res.status(500).send({ message: "image_url is must, but not found" });
     }
 
-    if (!image_url.match('(jpg|jpeg|gif|png)((\?.*)$|$)')) {
-      return res.send(500).send({ message: 'not a valid image_url is must' });
+    if (!isImageURL(imageURL.toString())) {
+      return res.status(500).send({ message: "not a valid image_url" });
     }
-    //call filterImageFromURL(image_url) to filter the image
-    const filteredpath = await filterImageFromURL(image_url);
-    console.log(filteredpath);
-    //send the resulting file in the response
-    return res.status(200).sendFile(filteredpath);
+    filterImageFromURL(imageURL.toString())
+      .catch((e) => {
+        return res.status(400).send({ message: "Unable to filter image." });
+      })
+      .then((filePath) => {
+        if (filePath.toString() == "error") {
+          return res.status(400).send({message: "Image url is invalid" });
+        }
+        return res
+          .on("finish", () => {
+            var fileArray: string[] = [filePath.toString()];
+            deleteLocalFiles(fileArray);
+          })
+          .status(200)
+          .sendFile(filePath.toString());
+      });
   });
-  
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
